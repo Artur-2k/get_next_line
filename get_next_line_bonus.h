3042889fdb787a7a5f39bc8067c061ac7f1b1760
Artur Silva <artuda-s@c2r6s4.42porto.com>
2024-05-22 16:49:09 +0100

    added bonus part. just added [] to big_buffer to store a big buffer for each fd xd. \n also deleted the makefile. this project doesnt need one kkkk

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 17:51:55 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/05/22 16:45:36 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/05/22 16:45:56 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdio.h>
 # include <stdlib.h>
